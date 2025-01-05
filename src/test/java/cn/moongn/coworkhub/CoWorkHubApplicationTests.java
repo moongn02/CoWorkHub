701f45ce7bf746fb98097df188a9ee74035eed1c
moongn02 <2269296084@qq.com>
2025-01-05 17:38:12 +0800

    Initial commit

---
+++ src/test/java/cn/moongn/coworkhub/CoWorkHubApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.moongn.coworkhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoWorkHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
